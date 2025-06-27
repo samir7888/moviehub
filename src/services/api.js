e9b8b1866e9ce879aee1ada8335d7443744bce0e
Sameer Basnet <basnetsameer78@gmail.com>
2025-06-27 12:12:13 +0545

    hihi

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ const BASE_URL = "https://api.themoviedb.org/3";
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
-  console.log(data)
   return data.results;
 };
 
